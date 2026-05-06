6d9a8d3e54bb536e87336c6c4b4da1cc5585ebf8
Abdullah Qamardeen Olamiteju <qamardeenabdullah@gmail.com>
2026-05-05 23:19:25 -0700

    fixing bmi

--- bmi.py
+++ bmi.py
@@ -1,23 +1,12 @@
-# Step 1: Get user input
-full_name = input("Enter your full name: ")
-birth_year = (input("Enter your year of birth"))
+# BMI CALCULATOR
+# 1. Get user input
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
 
-# Step 1: Get user input
-name_lower = full_name.upper()
-name_no_space = name_lower.replace(" ", "")
-name_length = len(name_no_space)
+# 2. Calculate BMI
+bmi = weight / (height ** 2)
 
-
-# Step 3: String slicing
-first3 = name_no_space[:3]         # First 3 letters
-year_suffix = birth_year[-2:]      # last 2 digits of birth year
-
-# Step 4: Create username with concatenation
-username = first3 + year_suffix
-
-# Step 5:Output with f-strings
-print("\n===== USER INFO =====")
-print(f"Original Name : {full_name}")
-print(f"UserName      : {username}")
-print(f"Name length   : {name_length} characters (excluding spaces)")
-print("=" * 17)
+# 3. Output the result
+print(f"\n===== BMI RESULT =====")
+print(f"Your calculated BMI is: {bmi:.2f}")
+print("======================")
